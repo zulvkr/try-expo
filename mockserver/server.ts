fdf90ec6f153ea92ad622d428aaa7ee0325057f3
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2024-02-16 09:10:05 +0700

    refactor server mock

--- mockserver/server.ts
+++ mockserver/server.ts
@@ -1,34 +1,36 @@
-import { Movie, User } from '@/api/types'
-import { Model, Response, createServer } from 'miragejs'
+import { Cast, Movie, User } from '@/api/types'
+import { Model, Response, belongsTo, createServer, hasMany } from 'miragejs'
 import { ModelDefinition } from 'miragejs/-types'
 
 const synopsis = `After graduating from Emory University, top student and athlete Christopher McCandless abandons his possessions, gives his entire $24,000 savings account to charity and hitchhikes to Alaska to live in the wilderness. Along the way, Christopher encounters a series of characters that shape his life.`
 
-const UserModel: ModelDefinition<User> = Model.extend<User>({
-  id: 0,
-  name: '',
-  email: '',
-  avatar: ''
+const picsumGenerator = (
+  id: number,
+  width: number = 280,
+  height: number = 400
+) => `https://picsum.photos/id/${id}/${width}/${height}`
+const avatarGenerator = (id: number) => `https://i.pravatar.cc/150?u=${id}`
+const randomId = () => Math.floor(Math.random() * 500) + 1
+const randomPicsum = () => picsumGenerator(randomId())
+const randomAvatar = () => avatarGenerator(randomId())
+
+const UserModel: ModelDefinition<User> = Model.extend({})
+
+const MovieModel: ModelDefinition<Partial<Movie>> = Model.extend({
+  casts: hasMany('cast')
 })
 
-const MovieModel: ModelDefinition<Partial<Movie>> = Model.extend<Movie>({
-  id: 0,
-  title: '',
-  image: '',
-  synopsis: '',
-  genres: [],
-  duration: 0,
-  director: '',
-  directorAvatar: '',
-  casts: []
+const CastModel: ModelDefinition<Partial<Cast>> = Model.extend({
+  movie: hasMany('recommendation')
 })
 
 export const makeServer = () => {
   window.server = createServer({
     models: {
-      users: UserModel,
-      recommendations: MovieModel,
-      upcomings: MovieModel
+      user: UserModel,
+      recommendation: MovieModel,
+      upcoming: MovieModel,
+      cast: CastModel
     },
 
     routes() {
@@ -91,165 +93,166 @@ export const makeServer = () => {
             genres: ['Adventure', 'Drama'],
             duration: 148,
             director: 'Sean Penn',
-            directorAvatar: 'https://i.pravatar.cc/150?u=seanpenn'
+            directorAvatar: randomAvatar()
           }
         ],
         recommendations: [
           {
+            id: 1,
             title: 'Into the Wild',
-            image: 'https://picsum.photos/id/211/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Adventure', 'Drama'],
             duration: 148,
             director: 'Sean Penn',
-            directorAvatar: 'https://i.pravatar.cc/150?u=seanpenn'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Shawshank Redemption',
-            image: 'https://picsum.photos/id/222/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Drama', 'Crime'],
             duration: 142,
             director: 'Frank Darabont',
-            directorAvatar: 'https://i.pravatar.cc/150?u=frankdarabont'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Godfather',
-            image: 'https://picsum.photos/id/212/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Science Fiction', 'Adventure', 'Action', 'Thriller'],
             duration: 175,
             director: 'Christopher Nolan',
-            directorAvatar: 'https://i.pravatar.cc/150?u=christophernolan'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Dark Knight',
-            image: 'https://picsum.photos/id/200/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Adventure', 'Drama'],
             duration: 152,
             director: 'Christopher Nolan',
-            directorAvatar: 'https://i.pravatar.cc/150?u=christophernolan'
+            directorAvatar: randomAvatar()
           },
 
           {
             title: 'Inception',
-            image: 'https://picsum.photos/id/275/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Action', 'Adventure', 'Sci-Fi'],
             duration: 148,
             director: 'Christopher Nolan',
-            directorAvatar: 'https://i.pravatar.cc/150?u=christophernolan'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'Pulp Fiction',
-            image: 'https://picsum.photos/id/25/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Crime', 'Drama'],
             duration: 154,
             director: 'Quentin Tarantino',
-            directorAvatar: 'https://i.pravatar.cc/150?u=quentintarantino'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Matrix',
-            image: 'https://picsum.photos/id/234/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Action', 'Sci-Fi'],
             duration: 136,
             director: 'Lana Wachowski',
-            directorAvatar: 'https://i.pravatar.cc/150?u=lanawachowski'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'Fight Club',
-            image: 'https://picsum.photos/id/232/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Drama'],
             duration: 136,
             director: 'Lana Wachowski',
-            directorAvatar: 'https://i.pravatar.cc/150?u=lanawachowski'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Lord of the Rings: The Fellowship of the Ring',
-            image: 'https://picsum.photos/id/233/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Adventure', 'Fantasy'],
             duration: 136,
             director: 'Lana Wachowski',
-            directorAvatar: 'https://i.pravatar.cc/150?u=lanawachowski'
+            directorAvatar: randomAvatar()
           }
         ],
         upcomings: [
           {
             title: 'The Revenant',
-            image: 'https://picsum.photos/id/101/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Adventure', 'Drama', 'Thriller'],
             director: 'Alejandro González Iñárritu',
-            directorAvatar: 'https://i.pravatar.cc/150?u=alejandro'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'Forrest Gump',
-            image: 'https://picsum.photos/id/102/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Drama', 'Romance'],
             director: 'Robert Zemeckis',
-            directorAvatar: 'https://i.pravatar.cc/150?u=robertzemeckis'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'Interstellar',
-            image: 'https://picsum.photos/id/103/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Adventure', 'Drama', 'Sci-Fi'],
             director: 'Christopher Nolan',
-            directorAvatar: 'https://i.pravatar.cc/150?u=christophernolan'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Silence of the Lambs',
-            image: 'https://picsum.photos/id/104/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Crime', 'Drama', 'Thriller'],
             director: 'Jonathan Demme',
-            directorAvatar: 'https://i.pravatar.cc/150?u=jonathandemme'
+            directorAvatar: randomAvatar()
           },
           {
             title: "Schindler's List",
-            image: 'https://picsum.photos/id/125/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Biography', 'Drama', 'History'],
             director: 'Steven Spielberg',
-            directorAvatar: 'https://i.pravatar.cc/150?u=stevenspielberg'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Green Mile',
-            image: 'https://picsum.photos/id/106/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Crime', 'Drama', 'Fantasy'],
             director: 'Frank Darabont',
-            directorAvatar: 'https://i.pravatar.cc/150?u=frankdarabont'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'City of God',
-            image: 'https://picsum.photos/id/107/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Crime', 'Drama'],
             director: 'Fernando Meirelles',
-            directorAvatar: 'https://i.pravatar.cc/150?u=fernandomeirelles'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'The Departed',
-            image: 'https://picsum.photos/id/108/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Crime', 'Drama', 'Thriller'],
             director: 'Martin Scorsese',
-            directorAvatar: 'https://i.pravatar.cc/150?u=martinscorsese'
+            directorAvatar: randomAvatar()
           },
           {
             title: 'Gladiator',
-            image: 'https://picsum.photos/id/109/280/400',
+            image: randomPicsum(),
             synopsis,
             genres: ['Action', 'Adventure', 'Drama'],
             director: 'Ridley Scott',
-            directorAvatar: 'https://i.pravatar.cc/150?u=ridleyscott'
+            directorAvatar: randomAvatar()
           }
         ]
       })
